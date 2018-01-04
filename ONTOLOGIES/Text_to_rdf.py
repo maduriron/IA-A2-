ab36812bd9ba57db3e65ea8bbb077dd4082ec5b8
FlorinCojocariu <cojocariu.cojocariu9@gmail.com>
2018-01-04 18:33:11 +0200

    Adaugat noua implementare pentru Text_to_rdf.py

--- ONTOLOGIES/Text_to_rdf.py
+++ ONTOLOGIES/Text_to_rdf.py
@@ -21,68 +21,87 @@ def test_Fdg_Parser(text):
     client = Client('http://nlptools.info.uaic.ro/WebFdgRo/FdgParserRoWS?wsdl')
     result = client.service.parseText(txt=text)
     return result
-# print test_Fdg_Parser('George are acadele')
 
 
-lista = []
+def generateConnetionPath(phrase,wordid,list):
+    x = wordid
+    word_id = str(x).split('.')[1]
+    for child in phrase:
+        if word_id == child.attrib["head"]:
+            list.append(child.attrib)
+            generateConnetionPath(phrase, child.attrib["id"], list)
+
+def connectionsBetweenWords(phrase,wordid):
+    x = wordid
+    prop_id = str(x).split('.')[0]
+    word_id = str(x).split('.')[1]
+    connected_words = []
+    for child in phrase:
+        if word_id == child.attrib["head"]:
+            connected_words.append(child.attrib)
+    return connected_words
+
+
+def list_to_text(list):
+    text = list[0]["LEMMA"]
+    for i in range(1, len(list)):
+        text += " "
+        text += list[i]["LEMMA"]
+    return text
 
 def parseXMLOutput():
-
+    return_list = []
     """Aici se pune(momentan) textul pe care dorim sa il transformam in ontologie"""
-    text_to_be_transformed = 'Neurochirurgia este o ramură a ştiinţelor medicale.'
+    text_to_be_transformed = """Stabilirea nivelului compresiunii este un criteriu clinic esenţial în vederea centrării explorării neuro-radiologice şi imagistice. Stabilirea nivelului se face pe baza găsirii dermatomului cel mai înalt cu sensibilitatea alterată, a nivelului motor, ambele corelate la topografia radiculo-vertebrală."""
     #Apelam functia de mai sus care ne parseaza texul si il tokenizeaza
 
-    result = test_Fdg_Parser(text_to_be_transformed)
-    root = ET.fromstring(result)
+    callback_result_from_Parser = test_Fdg_Parser(text_to_be_transformed)
+    root = ET.fromstring(callback_result_from_Parser)
+    print(callback_result_from_Parser)
     #Formam apoi un tuplu care contine: subiectul, verbul si un alt tuplu care contine obiectele
     for phrase in root:
-        grup = []
-        objects_list = []
+        current_list = []
         for child in phrase:
-            can_be_verb = 0
-            can_be_subject = 0
-            can_be_object = 0
+            if 'POS' in child.attrib and child.attrib['POS'] == 'VERB' or child.attrib["deprel"] == "ROOT":
+                current_list.append([child.attrib])
+                connected_words = connectionsBetweenWords(phrase, child.attrib["id"])
+                for word in connected_words:
+                    propozitie = []
+                    propozitie.append(word)
+                    generateConnetionPath(phrase,word["id"],propozitie)
+                    current_list.append(propozitie)
+        return_list.append(current_list)
 
-            for key, value in child.attrib.items():
-                if key == 'POS' and (value == 'NOUN' or value == 'PRONOUN'):
-                    can_be_subject = 1
-                if key == 'deprel' and value == 'sbj.' and can_be_subject == 1:
-                    grup.append((child.attrib["LEMMA"],child.attrib["deprel"]))
-                if key == 'POS' and value == 'VERB':
-                    can_be_verb = 1
-                if key == 'deprel' and value == 'ROOT' and can_be_verb == 1:
-                    grup.append((child.attrib["LEMMA"],child.attrib["deprel"]))
-                if key == 'POS' and (value == 'NOUN' or value == 'ADJECTIVE'):
-                    can_be_object = 1
-                if key == 'deprel' and (value == 'n.pred.' or value == 'c.d.' or value == 'a.adj.' or value == 'coord.'
-                                        or value == 'a.subst.' or value == 'prep.') and can_be_object == 1:
-                    objects_list.append((child.attrib["LEMMA"],child.attrib["deprel"]))
-        if len(objects_list) != 0:
-            grup.append(objects_list)
-        lista.append(tuple(grup))
-    return lista
+    new_list = []
+    for i in range(0, len(return_list)):
+        prop =[]
+        for j in range(0,len(return_list[i])):
+            lista = []
+            for x in range(0,len(return_list[i][j])):
+                lista.append((return_list[i][j][x]["LEMMA"],return_list[i][j][x]["deprel"],return_list[i][j][x]["id"]))
+            lista  = sorted(lista,key=lambda tup: tup[2])
+            prop.append(lista)
+        new_list.append(prop)
+    ret = []
+    for i in range(0, len(new_list)):
+        subiect = [None]
+        predicat = [None]
+        obiect = [None]
+        if(len(new_list[i])) > 0:
+            predicat = new_list[i][0]
+        else:
+            continue
+        for j in range(1,len(new_list[i])):
+            for k in range(0,len(new_list[i][j])):
+                if(new_list[i][j][k][1] == "sbj."):
+                    subiect = new_list[i][j]
+                    continue
+                if new_list[i][j][k][1] == 'n.pred.' or new_list[i][j][k][1] == 'c.d.' or new_list[i][j][k][1] == 'a.adj.' or new_list[i][j][k][1] == 'coord.' or new_list[i][j][k][1] == 'a.subst.' or new_list[i][j][k][1] == 'prep.':
+                    obiect = new_list[i][j]
+        ret.append((subiect,predicat,obiect))
+    for i in range(0,len(ret)):
+        print(ret[i])
+    return new_list
 
-#parseXMLOutput()
+x = parseXMLOutput()
 
-def to_rdf(list):
-    #Functie care duce tuplul nostru creat mai sus intr-un format rdf corespunzator
-    rdf = ET.Element('rdf:RDF')
-    rdf.set("xmlns:rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-    rdf.set("xmlns:dc", "http://purl.org/dc/elements/1.1/")
-    for value in list:
-        for obj in value[2]:
-            rdf_description = ET.SubElement(rdf, 'rdf:Description')
-            rdf_subject = ET.SubElement(rdf_description, 'rdf:'+value[0][1])
-            rdf_subject.text = value[0][0]
-            rdf_predicate = ET.SubElement(rdf_description, 'rdf:'+value[1][1])
-            rdf_predicate.text = value[1][0]
-            rdf_object = ET.SubElement(rdf_description, 'rdf:'+obj[1])
-            rdf_object.text = obj[0]
-
-    return tostring(rdf)
-
-
-# print(to_rdf(parseXMLOutput()))
-fd = open("Generated_rdf.xml", "wb")
-fd.write(b"<?xml version='1.0' encoding='utf-8'?>\n")
-fd.write(to_rdf(parseXMLOutput()))
