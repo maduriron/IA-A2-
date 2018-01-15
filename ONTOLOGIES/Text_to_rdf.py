a5af86693bed8458240438a497c8c22b4a87a773
FlorinCojocariu <cojocariu.cojocariu9@gmail.com>
2018-01-15 10:07:25 +0200

    Modificari aduse functiei pentru predicat

--- ONTOLOGIES/Text_to_rdf.py
+++ ONTOLOGIES/Text_to_rdf.py
@@ -11,7 +11,7 @@ def get_description_Fdg_parser():
         functiile oferite de serv web, insotite de parametri
     """
     client = Client('http://nlptools.info.uaic.ro/WebFdgRo/FdgParserRoWS?wsdl')
-    print(client)
+    #print(client)
 # get_description_Fdg_parser()
 
 def test_Fdg_Parser(text):
@@ -49,6 +49,25 @@ def list_to_text(list):
         text += list[i]["LEMMA"]
     return text
 
+def check_for_negation(phrase,wordid):
+    x = wordid
+    word_id = str(x).split('.')[1]
+    connected_words = []
+    for child in phrase:
+        if word_id == child.attrib["head"] and child.attrib["deprel"] == "neg." or child.attrib["deprel"] == "refl.":
+            connected_words.append(child.attrib)
+    return connected_words
+
+def check_for_two_roots(phrase,wordid):
+    x= wordid
+    word_id = str(x).split('.')[1]
+    connected_words = []
+    for child in phrase:
+        if word_id == child.attrib["head"] and child.attrib["deprel"] == "ROOT" and "POS" in child.attrib:
+            connected_words.append(child.attrib)
+    return connected_words
+
+
 def parseXMLOutput(striped_document):
     return_list = []
     """Aici se pune(momentan) textul pe care dorim sa il transformam in ontologie"""
@@ -58,12 +77,12 @@ def parseXMLOutput(striped_document):
     data = f.read().replace('\ufeff','').split('\n')
     f.close()
     data_collection = []
-
+    #data = []
     for text_to_be_transformed in data:
-        print(text_to_be_transformed)
+        #print(text_to_be_transformed)
         callback_result_from_Parser = test_Fdg_Parser(text_to_be_transformed)
         root = ET.fromstring(callback_result_from_Parser)
-        print(callback_result_from_Parser)
+        #print(callback_result_from_Parser)
         #Formam apoi un tuplu care contine: subiectul, verbul si un alt tuplu care contine obiectele
         for phrase in root:
             current_list = []
@@ -71,14 +90,19 @@ def parseXMLOutput(striped_document):
                 child.attrib["text"] = child.text
                 if 'EXTRA' in child.attrib and child.attrib['EXTRA'] == 'NotInDict':
                     continue
-                if 'POS' in child.attrib and child.attrib['POS'] == 'VERB' or child.attrib["deprel"] == "ROOT":
-                    current_list.append([child.attrib])
+                if 'POS' in child.attrib \
+                        and ((child.attrib['POS'] == 'VERB' and child.attrib['deprel'] == 'aux.')
+                        or(child.attrib['POS'] == 'VERB' and child.attrib['deprel'] == 'ROOT')
+                        or(child.attrib['POS'] == 'VERB' and child.attrib['deprel'] == 'ROOT' and check_for_two_roots(phrase,child.attrib["id"]))):
+                    #pprint(check_for_two_roots(phrase,child.attrib["id"]))
+                    current_list.append([child.attrib] + check_for_negation(phrase,child.attrib["id"]) + check_for_two_roots(phrase,child.attrib["id"]))
                     connected_words = connectionsBetweenWords(phrase, child.attrib["id"])
                     for word in connected_words:
                         propozitie = []
                         propozitie.append(word)
                         generateConnetionPath(phrase,word["id"],propozitie)
                         current_list.append(propozitie)
+            #pprint(current_list)
             return_list.append(current_list)
 
         new_list = []
@@ -88,12 +112,13 @@ def parseXMLOutput(striped_document):
                 lista = []
                 for x in range(0,len(return_list[i][j])):
                     lista.append((return_list[i][j][x]["text"],return_list[i][j][x]["deprel"],return_list[i][j][x]["id"]))
-                lista  = sorted(lista,key=lambda tup: tup[2])
+                lista  = sorted(lista,key=lambda tup: int(str(tup[2]).split('.')[1]))
                 prop.append(lista)
             new_list.append(prop)
 
-        ret = []
+
         for i in range(0, len(new_list)):
+            ret = []
             subiect = [None]
             predicat = [None]
             obiect = [None]
@@ -109,6 +134,7 @@ def parseXMLOutput(striped_document):
                     if new_list[i][j][k][1] == 'n.pred.' or new_list[i][j][k][1] == 'c.d.' or new_list[i][j][k][1] == 'a.adj.' or new_list[i][j][k][1] == 'coord.' or new_list[i][j][k][1] == 'a.subst.' or new_list[i][j][k][1] == 'prep.':
                         obiect = new_list[i][j]
             ret.append((subiect,predicat,obiect))
+
         data_collection.append(ret)
     return data_collection
 
@@ -127,12 +153,14 @@ def to_rdf(propList):
                 adjectiv = ""
                 for element_lista_adjectiv in value[2]:
                     adjectiv += element_lista_adjectiv[0] + " "
-
+                predicat = ""
+                for element_lista_predicat in value[1]:
+                    predicat += element_lista_predicat[0] + " "
                 rdf_description = ET.SubElement(rdf, 'rdf:Description')
                 rdf_subject = ET.SubElement(rdf_description, 'rdf:' + value[0][0][1])
                 rdf_subject.text = subiect.rstrip()
                 rdf_predicate = ET.SubElement(rdf_description, 'rdf:' + value[1][0][1])
-                rdf_predicate.text = value[1][0][0]
+                rdf_predicate.text = predicat.rstrip()
                 rdf_object = ET.SubElement(rdf_description, 'rdf:' + value[2][0][1])
                 rdf_object.text = adjectiv.rstrip() +'\n'
 
@@ -147,5 +175,7 @@ fd.write(b"<?xml version='1.0' encoding='utf-8'?>\n")
 fd.write(to_rdf(parseXMLOutput('sentences-proba.txt')))
 fd.close()
 
-#a = parseXMLOutput('sentences-proba.txt')
-#pprint(a)
+a = parseXMLOutput('sentences-proba.txt')
+pprint(a)
+
+
